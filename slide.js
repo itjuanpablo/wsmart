e15f546205d96fdff520366091b14d62cf80a60e
itjuanpablo <jp664165@gmail.com>
2023-04-23 15:58:41 -0300

    MOD: box shadow no slider e tempo de 40s de transição do slide

--- slide.js
+++ slide.js
@@ -1,41 +1,3 @@
-// var imgslide = document.getElementById('slide-img')
-
-// var images = [
-//   '/assets/Depoimentos/Gustavo.png',
-//   '/assets/Depoimentos/Ju-(Junia).png',
-//   '/assets/Depoimentos/Juliana.png',
-//   '/assets/Depoimentos/Silvia.png',
-//   '/assets/Depoimentos/Thaynara.png',
-//   '/assets/Depoimentos/Thi.png'
-// ]
-
-// var num = 0
-
-// function next() {
-//   num++
-//   if (num >= images.length) {
-//     num = 0
-//     imgslide.src = images[num]
-//   } else {
-//     imgslide.src = images[num]
-//   }
-// }
-
-// function back() {
-//   num--
-//   if (num <= 0) {
-//     num = images.length - 1
-//     currentImg.src = images[num]
-//   } else {
-//     currentImg.src = images[num]
-//   }
-// }
-
-// // Adicionar transição automática a cada 1 minuto
-// setInterval(function () {
-//   next()
-// }, 2000)
-
 var imgslide = document.getElementById('slide-img')
 var images = [
   '/assets/Depoimentos/Gustavo.png',
@@ -86,4 +48,4 @@ function updateActiveClass() {
 // Adicionar transição automática a cada 1 minuto
 setInterval(function () {
   next()
-}, 45000)
+}, 40000)
